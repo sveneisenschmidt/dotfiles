8c25421fb155b1bce9f6e89fdca78493ecd5dd23
Sven Eisenschmidt <sven.eisenschmidt@gmail.com>
2017-10-31 18:55:40 +0100

    Update

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,5 @@
-function fish_prompt --description 'Prompt ausgeben'
+function fish_prompt --description 'Write out the prompt'
+    # Just calculate these once, to save a few cycles when displaying the prompt
    if not set -q __fish_prompt_hostname
        set -g __fish_prompt_hostname (hostname -s)
    end
@@ -8,11 +9,9 @@ function fish_prompt --description 'Prompt ausgeben'
    end
 
    if not set -q __fish_prompt_cwd
-       set -g __fish_prompt_cwd (set_color red) #$fish_color_cwd)
+       set -g __fish_prompt_cwd (set_color $fish_color_cwd)
    end
 
-   echo
-   echo -n -s "$__fish_prompt_cwd" (prompt_pwd) (__fish_git_prompt)
-   echo
-   echo -n -s "$__fish_prompt_normal" '$ '
+   #echo -n -s (prompt_pwd) (__fish_git_prompt) "$__fish_prompt_normal" '> '
+   echo '> '
 end
--- README.md
+++ README.md
@@ -1,14 +1,4 @@
 dotfiles
 ========
 
-config files for zsh, tmux, ...
-
-## OS X
-
-### Animationen deaktivieren
-defaults write com.apple.dock expose-animation-duration -float 0
-killall Dock
-
-### Animationen aktivieren
-defaults delete com.apple.dock expose-animation-duration
-killall Dock
+config files for fish, tmux, iterm2, ...
